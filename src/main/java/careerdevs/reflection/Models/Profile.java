77dc4d7b84086a01e44fabb60f7bbb010751dd56
TBlais10 <blaislighting@gmail.com>
2022-05-17 22:50:05 -0400

    Fixing front end issue with profiles not being displayed

--- src/main/java/careerdevs/reflection/Models/Profile.java
+++ src/main/java/careerdevs/reflection/Models/Profile.java
@@ -11,7 +11,7 @@ import java.util.Set;
 public class Profile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
     private String fname;
     private String lname;
@@ -38,6 +38,14 @@ public class Profile {
         this.town = town;
         this.state = state;
         this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getFname() {
